05ea70c48fc0ed23f98606b3bb1e6050806cc373
Stepan <stepand555@gmail.com>
2026-04-23 19:59:31 +0300

    Fix parsers #4

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const {
 } = require("./services/parseKufarProduct");
 
 const {
-    parse5ElementProduct
+    parse5elementProduct
 } = require("./services/parse5elementProduct");
 
 const admin = require("firebase-admin");
